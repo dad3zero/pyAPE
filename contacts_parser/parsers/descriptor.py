d42dd81d0ec6337b68a793461cea4bdec34d6a6b
Darko Stankovski <darko.itpro@gmail.com>
2026-01-13 22:25:17 +0100

    Ajout de documentation

--- contacts_parser/parsers/descriptor.py
+++ contacts_parser/parsers/descriptor.py
@@ -3,5 +3,11 @@ from typing import TextIO
 from contacts_parser.parsers import pandas_toolbox as parser
 
 def describe(file_path: TextIO, separator: str | None = None) -> None:
+    """
+    Fonction utilitaire pour vérifier la structure du fichier csv.
+
+    :param file_path: Chemin vers le fichier csv
+    :param separator: Séparateur à utiliser
+    """
     dataset = parser.load_dataframe(file_path, sep=separator)
     print(list(dataset.columns))
